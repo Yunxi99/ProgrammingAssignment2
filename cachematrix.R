f6dc86c42d59f2a8284a7bd18059adfc1fca3e2c
Yunxi99 <67879537+Yunxi99@users.noreply.github.com>
2020-07-05 21:44:09 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -31,4 +31,32 @@ cacheSolve <- function(x, ...){       ## gets cache data
        inv
 } 
 
+## Show that how my function works
+> source("makeCacheMatrix.R)
+
+> pmatrix <- makeCacheMatrix(matrix(1:4,nrow=2,ncol=2))
+> pmatrix$get()
+     [,1] [,2]
+[1,]    1    3
+[2,]    2    4
+> pmatrix$getInverse()
+NULL
+> cacheSolve(pmatrix)
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> cacheSolve(pmatrix)
+getting cached data
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+> pmatrix$getInverse()
+     [,1] [,2]
+[1,]   -2  1.5
+[2,]    1 -0.5
+
+
+
+
+
         
